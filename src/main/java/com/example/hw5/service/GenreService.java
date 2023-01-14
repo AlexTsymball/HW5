78b5ad5cbf47ad489d7d92b4826fcaae755ab0c3
Sasha <alya.tsymbal@gmail.com>
2023-01-14 13:26:37 +0200

    initial

---
+++ src/main/java/com/example/hw5/service/GenreService.java
@@ -0,0 +1,10 @@
+package com.example.hw5.service;
+
+import com.example.hw5.dto.GenreDetailsDto;
+
+import java.util.List;
+
+public interface GenreService {
+
+    List<GenreDetailsDto> getAllGenre();
+}
---
+++ src/main/java/com/example/hw5/service/impl/GenreServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.hw5.service.impl;
+
+import com.example.hw5.data.GenreData;
+import com.example.hw5.dto.GenreDetailsDto;
+import com.example.hw5.repository.GenreRepository;
+import com.example.hw5.service.GenreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenreServiceImpl implements GenreService {
+
+    private final GenreRepository genreRepository;
+
+
+    @Override
+    public List<GenreDetailsDto> getAllGenre() {
+        return genreRepository.getAllGenre().stream()
+                .map(this::convertToDetails)
+                .toList();
+    }
+
+    private GenreDetailsDto convertToDetails(GenreData data) {
+        if (data == null){
+            return null;
+        }
+        return GenreDetailsDto.builder()
+                .id(data.getId())
+                .name(data.getName())
+                .build();
+    }
+
+}
